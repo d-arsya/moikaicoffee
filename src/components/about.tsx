7e95fc465ef115f346ea37a9acc37b7f5d804110
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-12-12 23:24:30 +0700

    feat: Image ornaments

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -4,7 +4,7 @@ import Pola from "../../public/pattern-1.png";
 export default function About() {
   return (
     <>
-      <section className="bg-black md:hidden relative">
+      <section className="bg-black sm:hidden relative">
         <div className="px-6 py-24">
           <h1 className="text-primary font-bold tracking-widest flex items-center gap-x-3">
             <span className="h-0.5 bg-white w-10 inline-block"></span>
@@ -30,7 +30,7 @@ export default function About() {
           className="absolute top-0 left-0 w-full h-full object-cover z-0"
         />
       </section>
-      <section className="relative bg-black hidden md:flex flex-row items-center">
+      <section className="relative bg-black hidden sm:flex flex-row items-center">
         <Image
           src={Pola}
           alt="pola"
