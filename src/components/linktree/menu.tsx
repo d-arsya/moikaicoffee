45fdf38cc075b4ac972f122d27d03da7182e1da8
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-12-15 10:21:15 +0700

    fix: Change menus with poster

--- src/components/linktree/menu.tsx
+++ src/components/linktree/menu.tsx
@@ -3,36 +3,27 @@ import Link from "next/link";
 import { Book } from "lucide-react";
 
 export default function Menu() {
-  const menus = [
-    "Rice Bowl",
-    "Nashville",
-    "Nasi Goreng Kecombrang",
-    "Kopi",
-    "Dawet Latte",
-  ];
-
   return (
     <section className="">
       <h2 className="text-center font-semibold text-lg mb-3">Our Menu</h2>
 
-      <div className="grid grid-flow-col grid-rows-2 auto-cols-[140px] gap-x-4 gap-y-5 overflow-x-auto px-6 pb-2">
-        {menus.map((menu, index) => (
-          <div
-            key={index}
-            className="rounded-xl overflow-hidden bg-primary shadow-md shadow-primary/30"
-          >
-            <Image
-              src={`/menus/menus-${index + 1}.jpg`}
-              alt={menu}
-              width={200}
-              height={200}
-              className="h-32 w-full object-cover rounded-md"
-            />
-            <p className="text-center text-sm font-medium py-1 text-white">
-              {menu}
-            </p>
-          </div>
-        ))}
+      <div className="grid grid-flow-col grid-rows-2 auto-cols-[170px] gap-x-4 gap-y-5 overflow-x-auto px-6 pb-2">
+        {Array.from({ length: 10 }, (_, i) => `Menu ${i + 1}`).map(
+          (menu, index) => (
+            <div
+              key={index}
+              className="rounded-xl overflow-hidden bg-primary shadow-md shadow-primary/30"
+            >
+              <Image
+                src={`/posters/poster-${index + 1}.png`}
+                alt={menu}
+                width={1080}
+                height={1920}
+                className="w-full aspect-9/16 object-cover rounded-md scale-105 transition-transform duration-500 ease-out hover:scale-100"
+              />
+            </div>
+          )
+        )}
       </div>
       <div className="p-6">
         <Link
